a36a4b261139b1f4bed122038a70a610df4b35ea
Malith Kuruppu <info.mkappworks@gmail.com>
2026-04-15 03:35:02 +0200

    fix(test): mock generalPrefsProvider in smoke test; avoid pending async timer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:code_bench_app/app.dart';
+import 'package:code_bench_app/features/settings/notifiers/general_prefs_notifier.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: CodeBenchApp()));
+    SharedPreferences.setMockInitialValues({});
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [generalPrefsProvider.overrideWith(() => _StubGeneralPrefsNotifier())],
+        child: const CodeBenchApp(),
+      ),
+    );
     expect(find.byType(MaterialApp), findsOneWidget);
   });
+}
+
+class _StubGeneralPrefsNotifier extends GeneralPrefsNotifier {
+  @override
+  Future<GeneralPrefsNotifierState> build() async {
+    return const GeneralPrefsNotifierState(
+      autoCommit: false,
+      deleteConfirmation: true,
+      terminalApp: '',
+      themeMode: ThemeMode.system,
+    );
+  }
 }
