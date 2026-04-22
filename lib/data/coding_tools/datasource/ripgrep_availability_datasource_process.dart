495ebfadd87d80ca3ce568228d6a19f6c30c7384
Malith Kuruppu <info.mkappworks@gmail.com>
2026-04-22 06:25:24 +0200

    fix(ripgrep): catch IOException in availability check (H5)

--- lib/data/coding_tools/datasource/ripgrep_availability_datasource_process.dart
+++ lib/data/coding_tools/datasource/ripgrep_availability_datasource_process.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import '../../../core/utils/debug_logger.dart';
+
 /// Checks whether the `rg` (ripgrep) binary is available on PATH.
 /// File name ends in `_process.dart` so the dart:io arch rule is satisfied.
 class RipgrepAvailabilityDatasource {
@@ -8,6 +10,9 @@ class RipgrepAvailabilityDatasource {
       final result = await Process.run('rg', ['--version']);
       return result.exitCode == 0;
     } on ProcessException {
+      return false;
+    } on IOException catch (e) {
+      dLog('[RipgrepAvailabilityDatasource] unexpected I/O error: $e');
       return false;
     }
   }
