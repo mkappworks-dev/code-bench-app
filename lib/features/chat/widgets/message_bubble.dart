ec1d82a79766519a2829383427b0c3f202e74c82
Malith Kuruppu <info.mkappworks@gmail.com>
2026-05-11 01:02:36 +0200

    refactor(chat): remove unused meta package import from message_bubble.dart

--- lib/features/chat/widgets/message_bubble.dart
+++ lib/features/chat/widgets/message_bubble.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meta/meta.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../../core/constants/theme_constants.dart';
